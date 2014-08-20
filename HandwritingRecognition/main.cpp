97337f0765b2e230955b5e9bad4df21afcb4da27
Andrew Wang <andrew@Andrews-MacBook-Pro.local>
2014-08-19 20:52:48 -0700

    Initial Commit

---
+++ HandwritingRecognition/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HandwritingRecognition
+//
+//  Created by Andrew Wang on 8/19/14.
+//  Copyright (c) 2014 Andrew Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
